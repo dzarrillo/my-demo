ca72836d34c9a5d47ebe37768bff9237b3c56951
Don Zarrillo Jr <donzarrillo@gmail.com>
2023-02-05 14:49:43 -0500

    add products component

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,8 +1,31 @@
-import { useState, UseEffect } from "react";
+import { useState, useEffect } from "react";
 import "./Products.css";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [myProducts, setMyProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums")
+      .then((response) => response.json())
+      .then((albums) => {
+        setMyProducts(albums);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>Products</div>
+      <div className="product-grid">
+        {myProducts.map((product) => {
+          return (
+            <div className="product-card">
+              <p> Product {product.id}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Products;
